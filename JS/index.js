13fee90d6f2dddb52acfad3f4a1d996d9df084b0
Andrew <ndrwrono2001@gmail.com>
2022-11-23 18:05:39 +0300

    update total

--- JS/index.js
+++ JS/index.js
@@ -36,7 +36,26 @@ function ready(){
 
 function removeCartItem(event){
     var buttonClicked = event.target
-    buttonClicked.parentElement.remove()
+    buttonClicked.parentElement.remove();
+    updatetotal();
 
+}
+
+function updatetotal(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = document.getElementsByClassName("cart-box");
+    var total = 0;
+
+    for(i=0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace("$",""));
+
+        var quantity = quantityElement.value;
+        total = total + price*quantity;
+
+
+    }
 }
 
