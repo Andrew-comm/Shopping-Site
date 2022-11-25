c97565dbdda45f775d2cc7f31ff39fbc4b632dde
Andrew <ndrwrono2001@gmail.com>
2022-11-26 01:10:47 +0300

    successfully added items to cart

--- JS/index.js
+++ JS/index.js
@@ -66,7 +66,7 @@ function quantityChanged(event){
 
 function addcartClicked(event){
     var button = event.target;
-    var shopProduct = button.parentElement
+    var shopProduct = button.parentElement;
     var title = shopProduct.getElementsByClassName("product-title")[0].innerText;
     var price = shopProduct.getElementsByClassName("price")[0].innerText;
     var productImg = shopProduct.getElementsByClassName("product-img")[0].src;
@@ -77,33 +77,34 @@ function addcartClicked(event){
 
 }
 function addProductToCart(title,price,productImg){
-    var cartShopBox = document.createElement("div");
+    var cartShopBox = document.createElement('div');
     cartShopBox.classList.add("cart-box");
-    var cartItems = doument.getElementsByClassName("cart-content")[0];
+    var cartItems = document.getElementsByClassName("cart-content")[0];
     var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
-    for(i=0; i < cartItemsNames.length; i++){
+    for(var i=0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title){
         alert("item already added to cart")
-
         return;
+        }
+
+       
     }
     
+    var cartBoxContent = `
+                            <img src="images/earbuds.png" alt="" class="cart-img">
+                            <div class="detail-box">
+                                <div class="cart-product-title">Earbuds</div>
+                                <div class="cart-price">$70.24</div>
+                                <input type="number" value="1" class="cart-quantity">
 
-}
-var cartBoxContent = `
-                        <img src="images/earbuds.png" alt="" class="cart-img">
-                        <div class="detail-box">
-                            <div class="cart-product-title">Earbuds</div>
-                            <div class="cart-price">$70.24</div>
-                            <input type="number" value="1" class="cart-quantity">
-
-                        </div>
-                        <i class="fa fa-trash" id="cart-remove" aria-hidden="true"></i>`;
+                            </div>
+                            <i class="fa fa-trash" id="cart-remove" aria-hidden="true"></i>`;
 
 cartShopBox.innerHTML = cartBoxContent
 cartItems.append(cartShopBox)
-cartShopBox.getElementsById("cart-remove")[0].addEventListener("click", removeCartItem)
-cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged)
-
+cartShopBox.getElementsByClassName('fa fa-trash')[0].addEventListener('click', removeCartItem)
+cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged)
+}
 
 
 
